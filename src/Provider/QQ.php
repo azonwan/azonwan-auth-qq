5b5ac2c1e6e04a48f7cfa475bab74c595e8c422a
wangxudong <wangxudong@innobuddy.com>
2016-01-28 10:08:50 +0800

    modify  name

--- src/Provider/QQ.php
+++ src/Provider/QQ.php
@@ -4,6 +4,7 @@ namespace Azonwan\Auth\QQ\Provider;
 
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
 use Psr\Http\Message\ResponseInterface;
 
